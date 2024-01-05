a1d5d3fe9bc363b751a4d354d94eff322bc7acd7
Vaibhav Shukla <124309121+vaibhav0925@users.noreply.github.com>
2024-01-05 02:16:18 +0000

    New Files

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,20 @@
+const account_ID = 12545896
+let account_Email = "vaibhav885@gmail.com"
+var account_Password = "123456"
+accountcity = "Jaipur"
+
+// account_ID = 5
+account_Email = "vaibhav_raja_ji@gmail.com"
+account_Password = "256398745"
+accountcity = "Vranasi"
+let account_state
+// account_state1
+// console.log(account_ID)
+console.table([account_Email,account_Password,accountcity,account_state])
+
+/* 
+Prefer not to use var
+because of issue in block scope and functional scopenode
+*/
+
+console.log(typeof accountcity)
