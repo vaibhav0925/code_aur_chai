6a280707bd7263ca80dc0319a236972235265c21
Vaibhav Shukla <124309121+vaibhav0925@users.noreply.github.com>
2024-01-05 02:52:27 +0000

    conversion

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+// let score = undefined
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber = Number (score)
+// console.log(typeof (valueInNumber))
+// console.log(valueInNumber)
+
+// let isloggedIn = "Raja_Ji"
+
+// let boolenIsLoggedIn = Boolean(isloggedIn)
+// console.log(boolenIsLoggedIn)
+
+
+let number = 55
+let stringnumber = String(number)
+
+console.log(stringnumber)
+console.log(typeof stringnumber)
