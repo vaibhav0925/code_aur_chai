d13a891617483e2e3aaf914f0c3756642742fe25
Vaibhav Shukla <124309121+vaibhav0925@users.noreply.github.com>
2024-01-07 20:42:09 +0000

    Comparison

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -13,8 +13,43 @@
 // console.log(boolenIsLoggedIn)
 
 
-let number = 55
-let stringnumber = String(number)
+// let number = 55
+// let stringnumber = String(number)
 
-console.log(stringnumber)
-console.log(typeof stringnumber)
+// console.log(stringnumber)
+// console.log(typeof stringnumber)
+
+//****************************************OPERATIONS******************************************* */
+
+// let value = 5
+// let negvalue = -value
+
+// console.log(negvalue)
+
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2/2)
+// console.log(2%2)
+// console.log(3**3)
+// console.log(2*2)
+
+
+// let str1 = "Hellow"
+// let str2 = " Raja_Ri"
+
+// let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 5)
+// console.log(1 + "5")
+// console.log("1" + 5 + 2)
+// console.log(1+ 5 + "2")
+// console.log(1 + "5" + 2)
+
+// console.log(1+(4*5)%4)
+
+
+console.log(+true)
+console.log(true)
+console.log(+"")
---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,12 @@
+// console.log(1>2)
+// console.log(5<8)
+// console.log(5!=8)
+// console.log(5==6)
+
+// console.log(undefined == 0)
+// console.log(undefined > 0)
+// console.log(undefined < 0)
+
+// === (Strick Check)
+
+console.log("5" === 5)
