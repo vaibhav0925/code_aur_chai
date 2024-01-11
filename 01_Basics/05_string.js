3c0ff5a804bdca3b4b66ab22e35a00133fa557ed
Vaibhav Shukla <124309121+vaibhav0925@users.noreply.github.com>
2024-01-11 07:50:12 +0000

    String

---
+++ 01_Basics/05_string.js
@@ -0,0 +1,10 @@
+const name = "Vaibhav"
+const repoCount = 50
+
+
+//console.log(name + repoCount + "Value")
+
+//``----- String interpulation (We create place holder and directely inject any variables)
+
+//This syntax is much more readable in backend development
+console.log(`Hello my name is ${name} and my repo couunt is ${repoCount}`)
