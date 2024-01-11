485eb4a26d23c4a129fccf3491a2505c0e511397
Vaibhav Shukla <124309121+vaibhav0925@users.noreply.github.com>
2024-01-11 13:31:50 +0000

    some modification

--- 01_Basics/05_string.js
+++ 01_Basics/05_string.js
@@ -5,6 +5,9 @@ const repoCount = 50
 //console.log(name + repoCount + "Value")
 
 //``----- String interpulation (We create place holder and directely inject any variables)
-
+// `` --(This symbol is known as Backticks)
 //This syntax is much more readable in backend development
 console.log(`Hello my name is ${name} and my repo couunt is ${repoCount}`)
+
+const gameName = new string("Vaibhav Raja_Ji")
+console.log(gameName[0])
