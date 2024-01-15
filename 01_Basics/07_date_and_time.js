9e33a864d85eb2f0854179dc966bf33250272e8c
Vaibhav Shukla <124309121+vaibhav0925@users.noreply.github.com>
2024-01-15 18:48:54 +0000

    Date And Time

--- 01_Basics/07_date_and_time.js
+++ 01_Basics/07_date_and_time.js
@@ -16,4 +16,21 @@ let myDate = new Date()
 // We can also declare in DD/MM/YYYY or YYYY/MM/DD format
 // let myCreatedDate = new Date("2023-01-25")
 let myCreatedDate = new Date("2017-10-25")
-console.log(myCreatedDate.toLocaleString())
+// console.log(myCreatedDate.toLocaleString())
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp)
+// console.log(myCreatedDate.getTime())
+// console.log(Math.floor(Date.now()/1000))
+
+
+let newDate = new Date()
+console.log(newDate)
+console.log(newDate.getMonth())
+console.log(newDate.getHours())
+
+// `${newDate} and in minutes is ${newDate.getMinutes}`
+
+newDate.toLocaleString('default',{
+    weekday: "long"
+})
