57d52c0eca5cb7895b5eb2c0150630da210f5fb6
Vaibhav Shukla <124309121+vaibhav0925@users.noreply.github.com>
2024-01-22 12:22:15 +0000

    Update

---
+++ 01_Basics/06_num_and_maths.js
@@ -0,0 +1,34 @@
+const score = 400
+// console.log(score)
+
+const balance = new Number(1000)
+// console.log(balance)
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+const otherNumber = 23.56325
+// console.log(otherNumber.toPrecision(4))
+
+const hundereds = 10000000
+// console.log(hundereds.toLocaleString())
+// console.log(hundereds.toLocaleString('en-IN'))
+
+
+//+++++++++++++++++++++++++++++++++++++  Mathematcs  ++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-8))
+// console.log(Math.round(4.39))
+// console.log(Math.ceil(4.16))
+// console.log(Math.floor(4.996))
+// console.log(Math.min(4,3,6,8))
+// console.log(Math.max(4,8,9,7))
+
+console.log(Math.random())
+console.log(Math.floor(Math.random()*10)+1)
+
+const max = 20
+const min = 10
+
+console.log(Math.floor(Math.random() * (max - min + 1))+ min)
