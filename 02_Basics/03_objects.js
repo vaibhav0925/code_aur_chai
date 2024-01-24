c270c559ddc9cd670dc48c651b441cfccaf262fa
Vaibhav Shukla <124309121+vaibhav0925@users.noreply.github.com>
2024-01-24 12:54:28 +0000

    Some addition

---
+++ 02_Basics/03_objects.js
@@ -0,0 +1,17 @@
+// singleton 
+// (This is also called constructor methods)
+
+//Object.create
+
+Object literals
+const JSUser = {
+    name : "Vaibhav",
+    location : "Varanasi",
+    email : "vaibhav@google.com",
+    isLoggedIn : false,
+    lastLoginDays : ["Saturday", "Monday"]
+    
+}
+
+console.log(JSUser.email)
+console.log(["email"])
