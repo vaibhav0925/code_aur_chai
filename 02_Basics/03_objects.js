31894e5be02251cbf1c2628fb51a409eb0a178b2
Vaibhav Shukla <124309121+vaibhav0925@users.noreply.github.com>
2024-01-24 20:21:06 +0000

    Some addition

--- 02_Basics/03_objects.js
+++ 02_Basics/03_objects.js
@@ -3,15 +3,38 @@
 
 //Object.create
 
-Object literals
+//Object literals
+
+const mySym = Symbol("key1")
+
 const JSUser = {
     name : "Vaibhav",
+    "full_name" : "Vaibhav Shukla",
+    age : 18,
     location : "Varanasi",
+    [mySym] : "myKey1",
     email : "vaibhav@google.com",
     isLoggedIn : false,
     lastLoginDays : ["Saturday", "Monday"]
     
 }
 
-console.log(JSUser.email)
-console.log(["email"])
+//console.log(JSUser.email)
+//console.log(JSUser.full_name)
+//console.log(JSUser["full_name"]) 
+//console.log(JSUser[mySym])
+
+JSUser.email = "vaibhav@openai.com"
+//Object.freeze(JSUser)
+JSUser.email = "vaibhav@microsoft.com"
+//console.log(JSUser)
+
+JSUser.greeting = function(){
+    console.log("Hello JS user")
+}
+
+JSUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`)
+}
+console.log(JSUser.greeting());
+console.log(JSUser.greetingTwo())
