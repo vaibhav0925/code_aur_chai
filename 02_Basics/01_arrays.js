07f530f54e4e84c60395c9b135fa5a110a480a49
Vaibhav Shukla <124309121+vaibhav0925@users.noreply.github.com>
2024-01-24 08:05:26 +0000

    Basics of Array

--- 02_Basics/01_arrays.js
+++ 02_Basics/01_arrays.js
@@ -1,7 +1,41 @@
 /// Arrays
 
-const myArr = [0,1,2,3,4,5,]
+const myArr = [0,1,2,3,4,5]
 
 const myHeros = ["shaktiman","naagraj"]
 const myArr2 = new Array(1,2,3,4,5)
-console.log(myArr[0])
+// console.log(myArr[5])
+
+// Array Methods
+
+// myArr.push(6,7,8)
+// console.log(myArr)
+// myArr.pop()
+// console.log(myArr)
+
+// myArr.unshift(11)
+// myArr.shift()
+// console.log(myArr)
+
+// console.log(myArr.includes(10))
+// console.log(myArr.indexOf(5))
+
+// const newArr = myArr.join()
+// console.log(myArr)
+// console.log(typeof newArr)
+// console.log(newArr)
+
+
+
+// slice, splice
+
+console.log("A", myArr)
+
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1)
+console.log("B", myArr)
+
+const myn2 = myArr.splice(1,4)
+console.log("C", myArr)
+console.log(myn2)
