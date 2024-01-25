da7f4b28cee9eb95475d1a7570be1994fc2cf65d
Vaibhav Shukla <124309121+vaibhav0925@users.noreply.github.com>
2024-01-25 08:37:21 +0000

    Functions

---
+++ 03_Basics/01_funtions.js
@@ -0,0 +1,13 @@
+/*   It means you have close certain code into pacakage and you can use it copy at anywhere for multiple time */
+
+function SayMyName(){
+    console.log("V")
+    console.log("A")
+    console.log("I")
+    console.log("B")
+    console.log("H")
+    console.log("A")
+    console.log("V")
+}
+
+SayMyName()
